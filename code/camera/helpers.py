c56921647f92614caf97fb7127ba4c91014ac215
dheera <dheera@dheera.net>
2020-10-04 20:06:28 +0000

    update

--- code/camera/helpers.py
+++ code/camera/helpers.py
@@ -5,6 +5,30 @@ import cv2
 import os
 import random
 import sys
+import PIL.Image
+import PIL.ExifTags
+
+def parse_maker_note(maker_note):
+    """Split the "maker note" EXIF field from a Raspberry Pi camera image into useful parameters"""
+    camera_parameters = {}
+    last_key = None
+    for p in maker_note.decode().split(" "):
+        # The maker note contains <thing>=<thing> entries, space delimited but with spaces in some values.
+        # So, if there is an = then we assume we've got key=value, but if there is no = sign, we append
+        # the current chunk to the latest value, because that's where it probably belongs...
+        if "=" in p:
+            last_key, v = p.split("=")
+            camera_parameters[last_key] = v
+        else:
+            camera_parameters[last_key] += " " + p
+    return camera_parameters
+
+def downsample(array, factor):
+    output = array[::factor, ::factor, :].copy()
+    for i in range(factor - 1):
+        for j in range(factor - 1):
+            output += array[(i+1)::factor, (j+1)::factor, :]
+    return output
 
 def read_raw(fn):
     file = open(fn, 'rb')
@@ -24,9 +48,25 @@ def read_raw(fn):
     data = unpacked_data
     return data
 
+def read_exif(fn):
+    img = PIL.Image.open(fn)
+    exif = {
+        PIL.ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in PIL.ExifTags.TAGS
+    }
+    for k in exif:
+        if type(exif[k]) is tuple and \
+            len(exif[k]) == 2 and \
+            type(exif[k][0]) is int and \
+            type(exif[k][1]) is int:
+            exif[k] = exif[k][0] / exif[k][1]
+    exif["MakerNote"] = parse_maker_note(exif.get("MakerNote"))
+    return exif
+
 def superpixel_debayer(img):
     b = img[0::2, 0::2] << 1
-    g = img[1::2, 0::2] + img[0::2, 1::2]
+    g = (img[1::2, 0::2] + img[0::2, 1::2])
     r = img[1::2, 1::2] << 1
     return np.dstack([b, g, r])
 
